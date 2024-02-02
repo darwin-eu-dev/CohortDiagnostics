33f009dfda749671f3b6c1e58a6a08d742ae55c5
Ger Inberg <ginberg@gmail.com>
2024-02-02 14:07:27 +0100

    fix test

--- tests/testthat/test-CDMConnector.R
+++ tests/testthat/test-CDMConnector.R
@@ -1,16 +1,24 @@
 test_that("cdm interface works", {
   exportFolder <- tempfile()
   cohortTable  <- "mycohort"
+  conceptCountsTable <- "conceptCounts"
+  cohortDefSet <- cohortDefinitionSet %>% dplyr::filter(.data$cohortId %in% c(14906))
   con <- DBI::dbConnect(duckdb::duckdb(), dbdir = CDMConnector::eunomia_dir())
   cdm <- CDMConnector::cdmFromCon(con, cdmSchema = "main", writeSchema = "main", cdmName = "eunomia")
-  cdm <- CDMConnector::generateCohortSet(cdm, cohortDefinitionSet, name = cohortTable)
+  cdm <- CDMConnector::generateCohortSet(cdm, cohortDefSet, name = cohortTable)
+  
+  CohortDiagnostics::createConceptCountsTable(connection = attr(cdm, "dbcon"),
+                                              cdmDatabaseSchema = "main",
+                                              conceptCountsDatabaseSchema = "main",
+                                              conceptCountsTable = conceptCountsTable)
   
   invisible(capture.output(
     executeDiagnosticsCdm(cdm = cdm,
-                          cohortDefinitionSet = cohortDefinitionSet,
+                          cohortDefinitionSet = cohortDefSet,
                           cohortTable = cohortTable,
+                          conceptCountsTable = conceptCountsTable,
                           exportFolder = exportFolder,
-                          useExternalConceptCountsTable = FALSE)
+                          useExternalConceptCountsTable = TRUE)
   ))
   
   expected_files <- c("cohort_count.csv", "cohort.csv", "database.csv", "executionTimes.csv", 
